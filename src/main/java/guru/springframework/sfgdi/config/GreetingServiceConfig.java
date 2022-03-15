b25a3666591f8bcb3066725759d6143638bac412
ejf071189 <evan.fredericks@gmail.com>
2022-03-15 12:01:01 -0500

    i18n services in java config

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,29 +1,52 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
+import guru.springframework.sfgdi.services.I18NEnglishService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by jt on 2/20/21.
  */
 @Configuration
 public class GreetingServiceConfig {
+  
+  @Profile({"EN"})
+  @Bean("i18nService")
+  I18NEnglishService i18NEnglishService() {
+    return new I18NEnglishService();
+  }
 
-    @Bean
-    ConstructorInjectedGreetingService constructorInjectedGreetingService(){
-        return new ConstructorInjectedGreetingService();
-    }
+  @Profile({"ES", "default"})
+  @Bean("i18nService")
+  I18NSpanishService i18NSpanishService() {
+    return new I18NSpanishService();
+  }
 
-    @Bean
-    PropertyInjectedGreetingService propertyInjectedGreetingService(){
-        return new PropertyInjectedGreetingService();
-    }
+  @Bean
+  @Primary
+  PrimaryGreetingService primaryGreeetingService() {
+    return new PrimaryGreetingService();
+  }
 
-    @Bean
-    SetterInjectedGreetingService setterInjectedGreetingService(){
-        return new SetterInjectedGreetingService();
-    }
+  @Bean
+  ConstructorInjectedGreetingService constructorInjectedGreetingService(){
+      return new ConstructorInjectedGreetingService();
+  }
+
+  @Bean
+  PropertyInjectedGreetingService propertyInjectedGreetingService(){
+      return new PropertyInjectedGreetingService();
+  }
+
+  @Bean
+  SetterInjectedGreetingService setterInjectedGreetingService(){
+      return new SetterInjectedGreetingService();
+  }
 }
